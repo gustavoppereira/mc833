7cc3a47608d136a0c043ec6c4aae210192ec7df3
Julio Barros <juliobarros@kissolutions.tech>
2019-04-02 22:47:29 -0300

    add client method selection and creating request object

--- projeto1/client.c
+++ projeto1/client.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
@@ -17,6 +18,31 @@
 #define PORT "3490" // the port client will be connecting to
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once
+
+#define MAXINPUTSIZE 50 // max size of input
+
+struct user {
+    char email[50];
+    char first_name[50];
+    char last_name[50];
+    char image[50];
+    char city[50];
+    char formation[50];
+    char skills[50];
+    char experience[50];
+};
+
+typedef struct user user;
+
+struct request {
+    char experience[50];
+    char formation[50];
+    char email[50];
+    char city[50];
+    int operation;
+};
+
+typedef struct request request;
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -32,12 +58,19 @@ int main(int argc, char *argv[])
 {
 	int sockfd, numbytes;
 	char buf[MAXDATASIZE];
+    char input[MAXINPUTSIZE];
 	struct addrinfo hints, *servinfo, *p;
-	int rv;
+	int rv, i;
+    int method_number = 6;
+    int selected_method = -1;
+    char * method_names[] = {"user-formation","hability-city","experience-add", "experience-email", "user-all", "user-email"};
+    char * method_fields[] = {"formation","city","experience", "email", "", "email"};
+    request send_request;
+    
 	char s[INET6_ADDRSTRLEN];
 
-	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
+	if (argc != 3) {
+	    fprintf(stderr,"usage: client hostname method\n");
 	    exit(1);
 	}
 
@@ -49,6 +82,32 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
+    
+    // reading method input
+    for(i=0; i<method_number; i++) {
+        if(!strcmp(method_names[i], argv[2])) {
+            selected_method = i;
+            break;
+        }
+    }
+    
+    if(selected_method == -1) {
+        fprintf(stderr, "failed to select method\n");
+        return 1;
+    }
+    
+    // get input for selected methods
+    if(selected_method != 4) {
+        send_request.operation = selected_method;
+        if(i==2) {
+            fprintf(stdout, "Input selected %s:\n", method_fields[selected_method+1]);
+            fgets (input, MAXINPUTSIZE, stdin);
+            strcpy(send_request.email,input);
+        }
+        fprintf(stdout, "Input selected %s:\n", method_fields[selected_method]);
+        fgets (input, MAXINPUTSIZE, stdin);
+        strcpy(send_request.experience,input);
+    }
 
 	// loop through all the results and connect to the first we can
 	for(p = servinfo; p != NULL; p = p->ai_next) {
@@ -78,7 +137,7 @@ int main(int argc, char *argv[])
 
 	freeaddrinfo(servinfo); // all done with this structure
 
-	if ((numbytes = send(sockfd, "PING", 4, 0)) == -1) {
+	if ((numbytes = send(sockfd, &send_request, sizeof(request), 0)) == -1) {
 		perror("send");
 		exit(1);
 	}
