c8f4f79a922bff9dd6b28b23867b13380a7d58a3
Julio Barros <juliobarros@kissolutions.tech>
2019-04-03 22:42:14 -0300

    fill db

--- projeto1/bump_db.c
+++ projeto1/bump_db.c
@@ -21,7 +21,14 @@ int main(void) {
 
   user input1 = {"buliojarros@gmail.com", "Julio", "Barros", "paulinho.jpg", "Taubate", "Maconheiro",  "Bolar Beck", "Varias porra"};
   user input2 = {"gustavoppereira@gmail.com", "Gustavo", "Pereira", "gus.jpg", "Rio", "Maconheiro Mirim",  "Fumar paiero", "Varios nada"};
-  user input3 = {"teste@email.com", "Teste", "1", "teste1.jpg", "Campinas", "Maconheiro",  "Fumar paiero", "Nada"};
+  user input3 = {"teste1@email.com", "Teste", "1", "teste1.jpg", "Campinas", "Maconheiro",  "Fumar paiero", "Nada"};
+  user input4 = {"teste2@email.com", "Teste", "2", "teste2.jpg", "Campinas", "Maconheiro Senior",  "Fumar paiero", "Nada"};
+  user input5 = {"teste3@email.com", "Teste", "3", "teste3.jpg", "Campinas", "Maconheiro",  "Fumar paiero", "Nada"};
+  user input6 = {"teste4@email.com", "Teste", "4", "teste4.jpg", "Campinas", "Maconheiro Senior",  "Fumar paiero", "Nada"};
+  user input7 = {"teste5@email.com", "Teste", "5", "teste5.jpg", "Campinas", "Maconheiro Deluxe",  "Fumar paiero", "Nada"};
+  user input8 = {"teste6@email.com", "Teste", "6", "teste6.jpg", "Campinas", "Maconheiro Pica",  "Fumar paiero", "Nada"};
+  user input9 = {"teste7@email.com", "Teste", "7", "teste7.jpg", "Campinas", "Jogador de Fifa",  "Jogar fifa", "Nada"};
+  user input10 = {"teste8@email.com", "Teste", "8", "teste8.jpg", "Campinas", "Maconheiro",  "Fumar paiero", "Nada"};
 
   file = fopen("database", "w");
 
@@ -33,6 +40,13 @@ int main(void) {
   fwrite(&input1, sizeof(user), 1, file);
   fwrite(&input2, sizeof(user), 1, file);
   fwrite(&input3, sizeof(user), 1, file);
+  fwrite(&input4, sizeof(user), 1, file);
+  fwrite(&input5, sizeof(user), 1, file);
+  fwrite(&input6, sizeof(user), 1, file);
+  fwrite(&input7, sizeof(user), 1, file);
+  fwrite(&input8, sizeof(user), 1, file);
+  fwrite(&input9, sizeof(user), 1, file);
+  fwrite(&input10, sizeof(user), 1, file);
 
   fclose(file);
 
